d928b92b425e0f4ec74c0f640339af4a5517c8f2
Hariprasath G <hari.hp12@gmail.com>
2018-09-26 00:53:03 +0200

    Update package docs

---
+++ R/cdoapi.R
@@ -0,0 +1,20 @@
+#' cdoapi: R Interface for the NOAA Climate Data Online web API
+#' 
+#' This package provides an R interface for the climate and weather
+#' data from the NOAA Climate Data Online web API
+#' 
+#' @section Objects:
+#' PLACEHOLDER
+#' 
+#' @section Authors:
+#' Hariprasath Govindarajan, Hector Plata and Julius Kittler
+#' 
+#' @section Maintainer:
+#' hargo729@student.liu.se
+#' 
+#' @section Reference:
+#' API: https://www.ncdc.noaa.gov/cdo-web/webservices/v2
+#'
+#' @docType package
+#' @name cdoapi
+NULL
--- R/data_api.R
+++ R/data_api.R
@@ -2,7 +2,6 @@
 #'
 #' @return
 #' @export
-#'
 cdo_api = function(){
   return(1)
 }
