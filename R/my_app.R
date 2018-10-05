2e43ffa03bde7ecea886354c12858b2da5506181
h4pZ <h4pz@protonmail.com>
2018-10-05 18:38:09 +0200

    Add responsive dropdown menus

--- R/my_app.R
+++ R/my_app.R
@@ -13,14 +13,40 @@ cities = get_cities()
 # Needs to be a function so nested dependencies are lazily loaded.
 # My ui.
 ui = function(){fluidPage(
-  titlePanel("TEST")
+  # Title of the shiny app.
+  titlePanel("CDO Visualization App"),
+  
+  sidebarLayout(position="left",
+                sidebarPanel(
+                  selectInput(inputId="country",
+                              label="Select Country Code",
+                              choices=unique(cities$country)),
+                  uiOutput("drop_down"),
+                  p("Get the country codes", 
+                    a("here", href="https://docs.opendata.aws/noaa-ghcn-pds/readme.html#lookup-table-of-country-codes"))),
+                
+                  
+  mainPanel("ASD"))
+
 )}
 
 # My server.
 server = function(input, output){
-  
+
+  output$drop_down = renderUI(
+    if (is.null(input$country)){
+      return()
+    }
+    else{
+      selectInput(inputId="city",
+                  label="Select a city",
+                  choices=cities$city[cities$country == input$country])
+    }
+  )
 }
 
 my_app = function(){
   shinyApp(ui=ui(), server=server)
 }
+
+my_app()
